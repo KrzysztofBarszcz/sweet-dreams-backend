11b548d01c7f2bb275a7afe42af596ff9f6c4223
Krzysztof Barszcz <krzysztof.barszcz@bettersolutions.pl>
2019-08-01 09:25:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sweet dreams - backend
+
+A backend of an internet bakery. Its purpose is to provide
+ a usage example of some of the most important Spring technologies 
---
+++ src/main/java/pl/krzyb/sweetdreamsbackend/SweetDreamsBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.krzyb.sweetdreamsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetDreamsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetDreamsBackendApplication.class, args);
+    }
+
+}
