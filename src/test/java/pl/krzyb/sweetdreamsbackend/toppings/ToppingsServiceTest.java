4e83ada5a3afaa106cdaf919133fb36d6d41ea72
Krzysztof Barszcz <keesio@wp.pl>
2019-08-05 22:15:18 +0200

    (#16) Correct all tests to use JUnit 5

--- src/test/java/pl/krzyb/sweetdreamsbackend/toppings/ToppingsServiceTest.java
+++ src/test/java/pl/krzyb/sweetdreamsbackend/toppings/ToppingsServiceTest.java
@@ -1,13 +1,12 @@
 package pl.krzyb.sweetdreamsbackend.toppings;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ToppingsServiceTest {
 
@@ -16,24 +15,24 @@ public class ToppingsServiceTest {
     @Test
     public void getOneShouldReturnTopping() {
         Optional<Topping> result = service.getTopping("Almonds");
-        Assert.assertThat(result.get().getName(), equalTo("Almonds"));
+        assertThat(result.get().getName(), equalTo("Almonds"));
     }
 
     @Test
     public void getAllShouldReturnAllToppings() {
         List<Topping> result = service.getToppings();
-        Assert.assertThat(result, hasSize(4));
+        assertThat(result, hasSize(4));
     }
 
     @Test
     public void getShouldIgnoreStringCase() {
         Optional<Topping> result = service.getTopping("PopPY");
-        Assert.assertThat(result.get().getName(), equalTo("Poppy"));
+        assertThat(result.get().getName(), equalTo("Poppy"));
     }
 
     @Test
     public void getNotExistingToppingShouldEmptyOptional() {
         Optional<Topping> result = service.getTopping("not existing");
-        Assert.assertTrue(result.isEmpty());
+        assertThat(result.isEmpty(), is(true));
     }
 }
